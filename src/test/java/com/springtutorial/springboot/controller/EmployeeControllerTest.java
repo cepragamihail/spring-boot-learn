a4a3895e6055c8b2c4a1ba6819608bc1ff1d1626
Mihail Cepraga <cepraga.mihail.leon@gmail.com>
2023-03-22 13:18:30 +0200

    Unit test to get Employee by Id REST API

--- src/test/java/com/springtutorial/springboot/controller/EmployeeControllerTest.java
+++ src/test/java/com/springtutorial/springboot/controller/EmployeeControllerTest.java
@@ -2,6 +2,8 @@ package com.springtutorial.springboot.controller;
 
 import com.springtutorial.springboot.dto.EmployeeDto;
 import com.springtutorial.springboot.service.EmployeeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
@@ -14,8 +16,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
+import java.util.Collections;
+
 @ExtendWith(SpringExtension.class)
-@WebFluxTest(controllers =  EmployeeController.class)
+@WebFluxTest(controllers = EmployeeController.class)
 class EmployeeControllerTest {
 
     @Autowired
@@ -23,13 +27,20 @@ class EmployeeControllerTest {
     @MockBean
     private EmployeeService employeeService;
 
-    @Test
-    public void givenEmployeeObject_whenSaveEmployee_thenReturnSavedEmployee() {
-        // given - pre-condition or set up
-        EmployeeDto employeeDto = new EmployeeDto();
+    private EmployeeDto employeeDto;
+
+    @BeforeEach
+    void prepare() {
+        employeeDto = new EmployeeDto();
         employeeDto.setFirstName("Mihail");
         employeeDto.setLastName("Cepraga");
         employeeDto.setEmail("cepraga.mihail.leon@gmail.com");
+    }
+
+    @Test
+    @DisplayName("Unit test to save Employee")
+    public void givenEmployeeObject_whenSaveEmployee_thenReturnSavedEmployee() {
+        // given - pre-condition or set up
         BDDMockito.given(employeeService.saveEmployee(ArgumentMatchers.any(EmployeeDto.class))).willReturn(Mono.just(employeeDto));
 
         // when - action or behaviour
@@ -46,4 +57,27 @@ class EmployeeControllerTest {
                 .jsonPath("$.lastName").isEqualTo(employeeDto.getLastName())
                 .jsonPath("$.email").isEqualTo(employeeDto.getEmail());
     }
+
+    @Test
+    @DisplayName("Unit test to get Employee by id")
+    public void givenEmployeeID_whenGetEmployee_thenReturnEmployeeObject() {
+        // given - pre-condition or set up
+        String employeeId = "123456";
+        BDDMockito.given(employeeService.getEmployee(employeeId))
+                .willReturn(Mono.just(employeeDto));
+
+        // when - action or behavior
+        WebTestClient.ResponseSpec response = webTestClient.get()
+                .uri("/api/employees/{id}", Collections.singletonMap("id", employeeId))
+                .exchange();
+
+        // then - verify the result or output
+        response.expectStatus().isOk()
+                .expectBody()
+                .consumeWith(System.out::println)
+                .jsonPath("$.firstName").isEqualTo(employeeDto.getFirstName())
+                .jsonPath("$.lastName").isEqualTo(employeeDto.getLastName())
+                .jsonPath("$.email").isEqualTo(employeeDto.getEmail());
+    }
+
 }
