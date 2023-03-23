e9ff4ed7dc12245dc5d0a088de305e52c064f0a8
Mihail Cepraga <cepraga.mihail.leon@gmail.com>
2023-03-23 14:25:09 +0200

    fix red test of endPoint greeting

--- src/main/java/com/springtutorial/springboot/controller/ConferenceController.java
+++ src/main/java/com/springtutorial/springboot/controller/ConferenceController.java
@@ -1,5 +1,6 @@
 package com.springtutorial.springboot.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class ConferenceController {
     @GetMapping("/about")
     public String about() {
         return "Join us online September 1-2!";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@AuthenticationPrincipal(expression = "username") String username) {
+        return "Hello, " + username + "!";
     }
 }
